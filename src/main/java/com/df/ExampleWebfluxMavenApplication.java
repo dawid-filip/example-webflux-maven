cb6c7d7259dd1268b345baaf6ba672293a4aad6f
dawid <dpilif@gmail.com>
2022-05-29 16:32:06 +0200

    master: init project.

---
+++ src/main/java/com/df/ExampleWebfluxMavenApplication.java
@@ -0,0 +1,13 @@
+package com.df;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleWebfluxMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleWebfluxMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/df/ExampleWebfluxMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.df;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleWebfluxMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
