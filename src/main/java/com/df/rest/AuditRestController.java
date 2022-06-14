e373d02cef4473fa92797b02fa83b135848891dd
dawid <dpilif@gmail.com>
2022-06-15 01:09:55 +0200

    master: minor code refactoring of Audit rest controller class.

--- src/main/java/com/df/rest/AuditRestController.java
+++ src/main/java/com/df/rest/AuditRestController.java
@@ -4,15 +4,14 @@ import com.df.entity.Audit;
 import com.df.service.AuditService;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.time.LocalDateTime;
+
+import static com.df.util.RequestUtility.getBasicResponseEntity;
 
 @RestController
 @RequestMapping("/api/v1/audit")
@@ -24,45 +23,30 @@ public class AuditRestController {
 
     @GetMapping
     public ResponseEntity<Flux<Audit>> getAll() {
-        return ResponseEntity.status(HttpStatus.OK)
-                .contentType(MediaType.APPLICATION_JSON)
-                .allow(HttpMethod.GET)
-                .body(auditService.getAll());
+        return getBasicResponseEntity(auditService.getAll());
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Mono<Audit>> getById(@PathVariable("id") Long id) {
-        return ResponseEntity.status(HttpStatus.OK)
-                .contentType(MediaType.APPLICATION_JSON)
-                .allow(HttpMethod.GET)
-                .body(auditService.getById(id));
+        return getBasicResponseEntity(auditService.getById(id));
     }
 
     @GetMapping("/between-ids")
     public ResponseEntity<Flux<Audit>> getBetweenIds(@RequestParam("startId") Long startId,
                                                      @RequestParam("endId") Long endId) {
-        return ResponseEntity.status(HttpStatus.OK)
-                .contentType(MediaType.APPLICATION_JSON)
-                .allow(HttpMethod.GET)
-                .body(auditService.getBetweenIds(startId, endId));
+        return getBasicResponseEntity(auditService.getBetweenIds(startId, endId));
     }
 
     @GetMapping("/like")
     public ResponseEntity<Flux<Audit>> getLikeEntityClass(@RequestParam("entityClass") String entityClass) {
-        return ResponseEntity.status(HttpStatus.OK)
-                .contentType(MediaType.APPLICATION_JSON)
-                .allow(HttpMethod.GET)
-                .body(auditService.getLikeEntityClass(entityClass));
+        return getBasicResponseEntity(auditService.getLikeEntityClass(entityClass));
     }
 
     // example: http://localhost:8080/api/v1/audit/between-audited-ons?startAuditedOn=2022-01-01T00:00:01.1&endAuditedOn=2022-01-01T00:00:02.1
     @GetMapping("/between-audited-ons")
     public ResponseEntity<Flux<Audit>> getBetweenAuditedOns(@RequestParam("startAuditedOn") LocalDateTime startAuditedOn,
                                                             @RequestParam("endAuditedOn") LocalDateTime endAuditedOn) {
-        return ResponseEntity.status(HttpStatus.OK)
-                .contentType(MediaType.APPLICATION_JSON)
-                .allow(HttpMethod.GET)
-                .body(auditService.getBetweenAuditedOns(startAuditedOn, endAuditedOn));
+        return getBasicResponseEntity(auditService.getBetweenAuditedOns(startAuditedOn, endAuditedOn));
     }
 
 }
--- src/main/java/com/df/util/RequestUtility.java
+++ src/main/java/com/df/util/RequestUtility.java
@@ -1,9 +1,21 @@
 package com.df.util;
 
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class RequestUtility {
+
+    public static <T> ResponseEntity<T> getBasicResponseEntity(T body) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_JSON)
+                .allow(HttpMethod.GET)
+                .body(body);
+    }
 
     public static Long convertTextToNumber(String valueText) {
         boolean isNumber = valueText
